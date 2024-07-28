530c629cf1e51c12b29f138df9821047d465bb41
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-07-29 01:16:53 +0300

    Handling requests and responses, understanding and working withroutes

--- Express.js with Node.js/my-express-server/server.js
+++ Express.js with Node.js/my-express-server/server.js
@@ -4,6 +4,28 @@ const app = express();
 
 const port = 3000;
 
+app.get("/", (req, res) => {
+  // console.log(req);
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact page</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send(
+    "<h2>Hello this is Collins Kinumbi, a self taught fullstack webdeveloper</h2>"
+  );
+});
+
+app.get("/hobbies", (req, res) => {
+  res.send(`<ul>
+    <li> Coding </li>
+    <li>Reading manga</li>
+    </ul>`);
+});
+
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
 });
