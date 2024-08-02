4e4e6df839e47ca6a24a1657a1225b8172f51291
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-08-02 14:51:12 +0300

    Challenge 10: capturing and storing form data from compose page

--- EJS/blog-challenge/app.js
+++ EJS/blog-challenge/app.js
@@ -42,7 +42,16 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  console.log(req.body.postTitle);
+  // console.log(req.body.postTitle);
+  // console.log(req.body.postBody);
+
+  const { postTitle, postBody } = req.body;
+
+  const post = {
+    postTitle,
+    postBody,
+  };
+  // console.log(post);
 });
 ////////////////////////////////////////////////////
 
