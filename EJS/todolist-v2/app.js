3ba4527d0d42fb810b1bc461a958245c51848627
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-08-05 13:37:25 +0300

    Setting up MongoDB and adding it in v2

--- EJS/todolist-v2/app.js
+++ EJS/todolist-v2/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const port = 3000;
 
-const items = ["buy food", "cook food", "eat food"];
-const workItems = [];
+// const items = ["buy food", "cook food", "eat food"];
+// const workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,6 +16,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+////////////////////////////////////////////////
+
+// Connecting to mongodb
+const url = "mongodb://localhost:27017/todolistDB";
+mongoose.connect(url);
+
+// items Schema
+const itemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please ensure you entered an Item."],
+  },
+});
+
+// Model
+const Item = mongoose.model("Item", itemsSchema);
+
+// Creating mongoose document(s)
+const item1 = new Item({
+  name: "Get gaming pc parts",
+});
+
+const item2 = new Item({
+  name: "Assemble gaming pc parts",
+});
+
+const item3 = new Item({
+  name: "Turn it on and start gaming!",
+});
+
+const defaultItems = [item1, item2, item3];
+
+//Insert into Items collection
+async function insertMany(arr) {
+  try {
+    await Item.insertMany(arr);
+    console.log("Items saved successfully");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+insertMany(defaultItems);
+
+////////////////////////////////////////////////
 
 // Home route
 app.get("/", (req, res) => {
