f69089b7cbd3a0e44e9258c257fbbc9bc86b1fde
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-08-18 12:37:03 +0300

    Connecting to MongoDB Atlas using Mongoose

--- Projects/MERN-stack-app/server/server.js
+++ Projects/MERN-stack-app/server/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import router from "./routes/workouts.js";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT;
@@ -13,15 +14,21 @@ app.use((req, res, next) => {
   next();
 });
 app.use(bodyParser.urlencoded({ extended: true }));
-
 /////////////////////////////////////////////
 
+//// Connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listening for requests
+    app.listen(port, () => {
+      console.log("connected to db and server running on port: " + port);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+/////////////////////////////////////////////
 ////Route handlers
 app.use("/api/workouts", router);
-
-/////////////////////////////////////////////
-
-// Listen for requests
-app.listen(port, () => {
-  console.log("Server running on port: " + port);
-});
