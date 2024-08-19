888126d79e87d44a447eb9bc654244fa84ca87be
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-08-19 14:07:16 +0300

    Deleting workouts from database

--- Projects/MERN-stack-app/frontend/src/components/WorkoutDetails.jsx
+++ Projects/MERN-stack-app/frontend/src/components/WorkoutDetails.jsx
@@ -1,5 +1,26 @@
+import { useWorkoutContext } from "../hooks/useWorkoutsContext";
+import axios from "axios";
+
 function WorkoutDetails({ workout }) {
-  const { title, load, reps, createdAt } = workout;
+  const { _id, title, load, reps, createdAt } = workout;
+
+  const { dispatch } = useWorkoutContext();
+
+  async function handleDelete() {
+    try {
+      const res = await axios.delete(`/api/workouts/${_id}`);
+
+      if (res.status === 200) {
+        dispatch({ type: "DELETE_WORKOUT", payload: res.data });
+      }
+    } catch (error) {
+      console.error(
+        "Error deleting workout:",
+        error.response?.data?.error || error.message
+      );
+    }
+  }
+
   return (
     <div className="workout-details">
       <h4>{title}</h4>
@@ -12,6 +33,7 @@ function WorkoutDetails({ workout }) {
         {reps}
       </p>
       <p>{createdAt}</p>
+      <span onClick={handleDelete}>delete</span>
     </div>
   );
 }
