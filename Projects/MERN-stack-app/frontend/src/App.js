8d185574a0f2718ef6e33c950af34082f540d556
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-08-19 11:18:05 +0300

    Setting up the react

--- Projects/MERN-stack-app/frontend/src/App.js
+++ Projects/MERN-stack-app/frontend/src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// components and pages
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
 function App() {
-  return <div></div>;
+  return (
+    <div classname="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
