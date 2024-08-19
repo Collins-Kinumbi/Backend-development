c466f5ab3bd764195a3d21c1bfcb69303ff17b6e
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-08-19 15:12:52 +0300

    Fixed minor home page bug

--- Projects/MERN-stack-app/frontend/src/pages/Home.jsx
+++ Projects/MERN-stack-app/frontend/src/pages/Home.jsx
@@ -7,28 +7,39 @@ import { useWorkoutContext } from "../hooks/useWorkoutsContext";
 function Home() {
   const { workouts, dispatch } = useWorkoutContext();
 
-  // Fetch from workouts api
+  // Fetch from workouts API
   useEffect(() => {
     async function fetchWorkouts() {
-      const res = await axios.get("/api/workouts");
-      const { data } = res;
-      console.log(data);
+      try {
+        const res = await axios.get("/api/workouts");
+        const { data } = res;
+        console.log(data);
 
-      if (res.status === 200) {
-        dispatch({ type: "SET_WORKOUTS", payload: data });
+        if (res.status === 200) {
+          dispatch({ type: "SET_WORKOUTS", payload: data });
+        } else {
+          console.error(`Error fetching workouts: ${res.status}`);
+        }
+      } catch (error) {
+        console.error("Failed to fetch workouts:", error);
       }
     }
     fetchWorkouts();
   }, [dispatch]);
+
+  if (!workouts) {
+    return <h4 style={{ textAlign: "center" }}>Loading...</h4>;
+  }
+
   return (
     <div className="home">
       <div className="workouts">
         {workouts.length === 0 ? (
           <h4 style={{ textAlign: "center" }}>No workouts here yet</h4>
         ) : (
-          workouts.map((workout) => {
-            return <WorkoutDetails key={workout._id} workout={workout} />;
-          })
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))
         )}
       </div>
       <WorkoutForm />
